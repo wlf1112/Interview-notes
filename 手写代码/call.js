e9fd4588a849b16eee013d26c4d40285603d94fa
WangLifeng <2322825963@qq.com>
2021-02-25 09:36:50 +0800

    Update call.js

--- 手写代码/call.js
+++ 手写代码/call.js
@@ -1,9 +1,9 @@
-//apply和call的区别在于：apply接收的参数是数组
-Function.prototype.myApply = function (context, args) {
+// call的本质：1.改变了当前函数的this指向   2.执行当前函数
+Function.prototype.myCall = function (context, ...args) {
   const fn = Symbol('fn');//用Symbol是为了防止覆盖self上的方法
   context = context || window;
-  context.fn = this;
-  let result = eval('context.fn(...args)');
-  delete context.fn;//删除声明的fn属性
-  return result;//返回执行结果
+  context.fn = this;//this就是调用myCall的方法
+  const result = eval('context.fn(...args)');//执行函数
+  delete context[fn];//删除声明的fn属性
+  return result;
 }
