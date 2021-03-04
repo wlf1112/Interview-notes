c6f3959b52fd975c093d52da345d81ebe9d14287
WangLifeng <2322825963@qq.com>
2021-03-04 11:45:57 +0800

    Update call.js

--- 手写代码/call.js
+++ 手写代码/call.js
@@ -1,9 +1,28 @@
-// call的本质：1.改变了当前函数的this指向   2.执行当前函数
-Function.prototype.myCall = function (context, ...args) {
-  const fn = Symbol('fn');//用Symbol是为了防止覆盖self上的方法
-  context = context || window;
-  context.fn = this;//this就是调用myCall的方法
-  const result = eval('context.fn(...args)');//执行函数
-  delete context[fn];//删除声明的fn属性
+// call的本质：1.改变了当前函数的this指向  
+//             2.执行当前函数
+
+//ES5写法
+Function.prototype.mycall = function () {
+  let context = context || window;
+  let fn = Symbol('fn');
+  context.fn = this;
+
+  let args = [];
+  for (let i = 1; i < arguments.length; i++) {
+    args.push('arguments[' + i + ']');
+  }
+  let result = eval('context.fn(' + args + ')');
+  delete context.fn;
   return result;
 }
+
+//ES6写法
+Function.prototype.myCall = function (context, ...args) {
+  const fn = Symbol('fn') //用Symbol是为了防止覆盖self上的方法
+  context = context || window
+  context.fn = this //this就是调用myCall的方法
+  const result = eval('context.fn(...args)') //执行函数
+  delete context[fn] //删除声明的fn属性
+  return result
+}
+
