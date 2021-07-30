22e66722da8ab6e4afa29fb58c4c88b97ac5849c
wanglifeng <wanglifeng@cont.com.cn>
2021-07-30 11:03:59 +0800

    modify rollup-demo

--- ES6/rollup-demo/rollup.config.js
+++ ES6/rollup-demo/rollup.config.js
@@ -1,8 +1,8 @@
-import babel from "rollup-plugin-babel"
-import resolve from "rollup-plugin-node-resolve"
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'src/index.js',
+  input: 'src/index.js',
   format: 'umd',
   plugins: [
     resolve(),
--- ES6/rollup-demo/src/index.js
+++ ES6/rollup-demo/src/index.js
@@ -1,11 +1,34 @@
-import util1 from './util1'
-import {
-  fn1,
-  fn2
-} from './util2'
+// import util1 from './util1'
+// import {
+//   fn1,
+//   fn2
+// } from './util2'
 
-console.log(util1);
-fn1()
-fn2()
+// console.log(util1);
+// fn1()
+// fn2()
 
 // [1, 2, 4].map(item => item + 1)
+
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+  eat() {
+    console.log(this.name + 'eat');
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name)
+    this.name = name
+  }
+  bar() {
+    console.log(this.name + 'bar');
+  }
+}
+
+const hashiqi = new Dog('哈士奇')
+hashiqi.eat()
+hashiqi.bar()
