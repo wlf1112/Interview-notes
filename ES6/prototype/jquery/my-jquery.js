09260edd82495bfe0cc9a98436abc2d1087893f2
wanglifeng <wanglifeng@cont.com.cn>
2021-08-02 14:37:02 +0800

    add jquery zepto

--- ES6/prototype/jquery/my-jquery.js
+++ ES6/prototype/jquery/my-jquery.js
@@ -1,5 +1,4 @@
 (function (window) {
-
   var jQuery = function (selector) {
     return new jQuery.fn.init(selector)
   }
@@ -9,24 +8,22 @@
       alert('css')
     },
     html: function (value) {
-      return '这是一个html的模拟'
+      return '模拟html'
     }
   }
 
   var init = jQuery.fn.init = function (selector) {
     var slice = Array.prototype.slice
     var dom = slice.call(document.querySelectorAll(selector))
-
     var i, len = dom ? dom.length : 0
     for (i = 0; i < len; i++) {
       this[i] = dom[i]
     }
     this.length = len
-    this.selector = selector || ''
+    this.selector = selector
   }
 
   init.prototype = jQuery.fn
 
   window.$ = jQuery
-
 })(window)
--- ES6/prototype/zepto/my-zepto.js
+++ ES6/prototype/zepto/my-zepto.js
@@ -1,3 +1,39 @@
 (function (window) {
+  var zepto = {}
 
-})
+  function Z(dom, selector) {
+    var i, len = dom ? dom.length : 0
+    for (i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
+  }
+
+  zepto.Z = function (dom, selector) {
+    return new Z(dom, selector)
+  }
+
+  zepto.init = function (selector) {
+    var slice = Array.prototype.slice
+    var dom = slice.call(document.querySelectorAll(selector))
+    return zepto.Z(dom, selector)
+  }
+
+  var $ = function (selector) {
+    return zepto.init(selector)
+  }
+
+  window.$ = $
+
+  $.fn = {
+    css: function (key, value) {
+      alert('css')
+    },
+    html: function (value) {
+      alert('html')
+      return '模拟html'
+    }
+  }
+  Z.prototype = $.fn //构造函数的原型上的方法，所有的实例都可以访问到
+})(window)
