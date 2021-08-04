9312955da8052550ba6bd9da7b751a6b40ff21a5
wanglifeng <wanglifeng@cont.com.cn>
2021-08-04 16:02:37 +0800

    modify promise

--- ES6/async/promise.js
+++ ES6/async/promise.js
@@ -1,6 +1,6 @@
 function loadImg(src) {
-  const promise = new Promise(function (resolve, reject) {
-    const img = document.createElement('img')
+  var promise = new Promise(function (resolve, reject) {
+    var img = document.createElement('img')
     img.onload = function () {
       resolve(img)
     }
@@ -12,15 +12,13 @@ function loadImg(src) {
   return promise
 }
 
-
-var src = 'https://www.imooc.com/static/img/index/logo2020.png'
+var src = "https://www.imooc.com/static/img/index/logo2020.png";
 var result = loadImg(src)
 result.then(function (img) {
-  console.log(img.width)
-}, function () {
-  console.log('fail')
-})
-
-result.then(function (img) {
   console.log(img.height)
+  return img
+}).then(function (img) {
+  console.log(img.width)
+}).catch(function (ex) {
+  console.log(ex)
 })
