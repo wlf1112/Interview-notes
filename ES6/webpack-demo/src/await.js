a33b83be2ca2f3d68fce19e1a0910caa6cff4dff
wanglifeng <wanglifeng@cont.com.cn>
2021-08-05 11:39:10 +0800

    add async/await

--- ES6/webpack-demo/src/await.js
+++ ES6/webpack-demo/src/await.js
@@ -1,16 +1,3 @@
-// import util1 from './util1'
-// import {
-//   fn1,
-//   fn2
-// } from './util2'
-
-// console.log(util1);
-// fn1()
-// fn2()
-
-// [1, 2, 4].map(item => item + 1)
-
-
 import 'babel-polyfill'
 
 function loadImg(src) {
